6de64cb048fd0fb3a80d180e31d6975944c784b4
Igor Volchenok <checklogon@gmail.com>
2018-11-22 01:49:33 +0700

    navbar and router added

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,101 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar class="navbar-top navbar-fixed-top" toggleable="md" type="light" role="navigation">
+      <b-container>
+        
+        <!-- Logo -->
+        <b-navbar-brand to="/" tag="span" style="cursor:pointer">my SaaS</b-navbar-brand>
+
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+        <b-collapse is-nav id="nav_collapse">
+
+          <!-- Center aligned nav item -->
+          <b-navbar-nav class="ml-md-auto">
+            <b-nav-item to="agencies" title="Agencies" >Agencies</b-nav-item>
+
+            <b-nav-item to="in-house">In-House</b-nav-item>
+
+            <b-nav-item-dropdown text="Features">
+              <b-dropdown-item to="#">All Features</b-dropdown-item>
+              <b-dropdown-item to="#">Communicate</b-dropdown-item>
+              <b-dropdown-item to="#">File Management</b-dropdown-item>
+              <b-dropdown-item to="#">Automation</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item to="join">Join</b-nav-item>
+
+            <b-nav-item to="blog">Blog</b-nav-item>
+
+            <b-nav-item-dropdown text="More">
+              <b-dropdown-item to="#">Contact</b-dropdown-item>
+              <b-dropdown-item to="#">About Us</b-dropdown-item>
+              <b-dropdown-item to="#">Careers</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav item -->
+          <b-nav class="ml-auto button-green" pills>
+            <b-nav-item class="login" to="login" active>Log in</b-nav-item>
+          </b-nav>
+
+        </b-collapse>
+      </b-container>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import './assets/scss/common.scss';
+@import './assets/scss/variables.scss';
+
+// ----------------------------------- Navigations start
+
+.navbar-light .navbar-nav .nav-link {
+  font-size: 14px;
+
+  &:hover, &:focus{
+    color: $hover !important;
+  }
 }
-#nav {
-  padding: 30px;
+
+.login {
+ 
+  // padding-left: 11px;
+  // padding-right: 11px;
+  // height: 32px;
+  line-height: 0;
+  // margin-top: 20px;
+  // color: #fff;
+  // display: inline-block;
   a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    background-color: $accent !important;
+    font-size: 14px;
+    font-weight: 600;
+    padding: 1rem 1rem;
+    &:hover {
+      background-color: #03940F!important;
+      border-top-color: black !important;
+      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.07), 0 4px 15px 0 rgba(0, 0, 0, 0.12);
+      transition: 0.4s;
     }
   }
 }
+
+.navbar-toggler {
+  border: none !important;
+  padding: 4px 0 !important;
+    
+  &:focus {
+    outline: 0;
+  }
+}
+
+.dropdown-toggle::after {
+ border-top-color: #cccccc !important;
+ vertical-align: 0.09em !important;
+}
+
+// ----------------------------------- Navigations End
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home/Home.vue';
+import Agencies from '@/views/Agencies/Agencies.vue';
+import InHouse from '@/views/InHouse/InHouse.vue';
+import Features from '@/views/Features/Features.vue';
+import Join from '@/views/Join/Join.vue';
+import Blog from '@/views/Blog/Blog.vue';
+import More from '@/views/More/More.vue';
+import Login from '@/views/Login/Login.vue';
 
 Vue.use(Router)
 
@@ -14,12 +21,50 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/agencies',
+      name: 'Agencies',
+      component: Agencies
+    },
+    {
+      path: '/in-house',
+      name: 'inHouse',
+      component: InHouse
+    },
+
+    // {
+    //   path: '/features/',
+    //   name: 'Features',
+    //   component: Features
+    // },
+
+    {
+      path: '/join',
+      name: 'Join',
+      component: Join
+    },
+
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: Blog
+    },
+    {
+      path: '/more',
+      name: 'More',
+      component: More
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })
